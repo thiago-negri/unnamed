a8a9826edcf9d3eae8408096ab39a43320c0c1f3
Thiago B. Negri <evohunz@gmail.com>
2017-10-26 00:12:06 -0200

    Set GLFW error callback

--- src/main.c
+++ src/main.c
@@ -5,28 +5,17 @@ static int width = 640;
 static int height = 480;
 static const char *title = "Hello, World";
 
+GLFWwindow *init(void);
+void glfw_error_callback(int, const char *);
+
 int main(void)
 {
     GLFWwindow *window;
 
-    // Initialize GLFW
-    if (glfwInit() != GLFW_TRUE)
-    {
-        fprintf(stderr, "Could not initialize GLFW.\n");
-        return 1;
-    }
+    window = init();
 
-    // Create window
-    window = glfwCreateWindow(width, height, title, NULL, NULL);
     if (window == NULL)
-    {
-        fprintf(stderr, "Could not open window.\n");
-        glfwTerminate();
         return 1;
-    }
-
-    // Display window
-    glfwMakeContextCurrent(window);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -37,5 +26,39 @@ int main(void)
 
     glfwTerminate();
     return 0;
+}
+
+GLFWwindow *init(void)
+{
+    GLFWwindow *window;
+
+    // Set GLFW error callback
+    glfwSetErrorCallback(glfw_error_callback);
+
+    // Initialize GLFW
+    if (glfwInit() != GLFW_TRUE)
+    {
+        fprintf(stderr, "Could not initialize GLFW.\n");
+        return NULL;
+    }
+
+    // Create window
+    window = glfwCreateWindow(width, height, title, NULL, NULL);
+    if (window == NULL)
+    {
+        fprintf(stderr, "Could not open window.\n");
+        glfwTerminate();
+        return NULL;
+    }
+
+    // Display window
+    glfwMakeContextCurrent(window);
+
+    return window;
+}
+
+void glfw_error_callback(int error, const char *description)
+{
+    fprintf(stderr, "GLFW error %i: %s\n", error, description);
 }
 
